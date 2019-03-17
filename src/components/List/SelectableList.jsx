982cb8262755d11caff0755b037d90e4840181ee
raeesapps <raees.aamir@yandex.com>
2019-03-17 07:13:53 +0000

    Fixed selectable bug with disabled items

--- src/components/List/SelectableList.jsx
+++ src/components/List/SelectableList.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const SelectableList = props => {
   const { items } = props;
   const [selectedItems, setSelectedItems] = useState(items.map(() => false));
-  const onSelectItem = selectedIdx => setSelectedItems(items.map((item, idx) => selectedIdx === idx));
+  const onSelectItem = selectedIdx => setSelectedItems(items.map((item, idx) => !item.props.hasOwnProperty("disabled") && selectedIdx === idx));
 
   const listItems = items.map((item, idx) => {
     const state = selectedItems[idx] ? "active" : "inactive";
