c51bc3d1b1b7e31ada9950fb881857b9f22f8f4b
raeesapps <raees.aamir@yandex.com>
2019-03-23 18:04:36 +0000

    Connected controller to global state

--- src/newviews/Controller/Controller.jsx
+++ src/newviews/Controller/Controller.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 
 const Controller = props => {
 
 };
 
-export default Controller;
+const mapStateToProps = props => {
+  const { selectedRobot } = props.robotState;
+  const { loginToken } = props.auth;
+  return {
+    selectedRobot,
+    loginToken
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Controller);
