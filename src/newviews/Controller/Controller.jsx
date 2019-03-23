2fb784914e97d3a17faf924c9cbfe7ef0f5330b4
raeesapps <raees.aamir@yandex.com>
2019-03-23 18:12:22 +0000

    Added HTTP methods to controller

--- src/newviews/Controller/Controller.jsx
+++ src/newviews/Controller/Controller.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import { connect } from "react-redux";
 
+import moveRobot from "../../http/move_robot";
+import Gamepad from "../../components/Gamepad/Gamepad";
+
 const Controller = props => {
+
+  const { selectedRobot, loginToken } = props;
+  const [alertVisible, showAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState(false);
+
+
+  const onMove = async direction => {
+    const { loginToken, selectedRobot } = props;
+    const response = await moveRobot(loginToken, direction, selectedRobot.id);
+
+    showAlert(true);
+    if (response.status === 200) {
+      alertMessage("Successfully moved robot!");
+    } else {
+      const body = await response.json();
+      setAlertMessage(body.message);
+    }
+  };
+
+  const createGamepad = () => {
+    return (
+      <Gamepad
+        forward={this.onMove.bind(this, "forward")}
+        backward={this.onMove.bind(this, "backward")}
+        armdown={this.onMove.bind(this, "armdown")}
+        armup={this.onMove.bind(this, "armup")}
+        left={this.onMove.bind(this, "left")}
+        right={this.onMove.bind(this, "right")}
+        brake={this.onMove.bind(this, "brake")}
+      />
+    );
+  };
+
+  return (
+    <div className="content">
+
+    </div>
+  )
 
 };
 
