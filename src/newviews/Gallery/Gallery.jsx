e3fba31ec63ac8b5324374b448087c4a0ca38df7
raeesapps <raees.aamir@yandex.com>
2019-03-25 17:59:40 +0000

    Improved gallery grid positioning

--- src/newviews/Gallery/Gallery.jsx
+++ src/newviews/Gallery/Gallery.jsx
@@ -3,6 +3,7 @@ import Card from "../../components/Card/Card";
 import data from "../../assets/components/Grid/data";
 
 const Gallery = props => {
+  
   return (
     <div className="content">
       <div className="container-fluid">
@@ -11,11 +12,11 @@ const Gallery = props => {
             <Card
               title={"Photo gallery"}
               content={<div className="container-fluid">
-                <div className="row">
+                <center><div className="row">
                   {
-                    data.map(image => <div className="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray"><img src={image.img} alt={image.title} /></div>)
+                    data.map(image => <div className="col-md-6"><img style={{height: "50%", width: "80%", marginBottom: "25px"}} src={image.img} alt={image.title} /></div>)
                   }
-                </div>
+                </div></center>
               </div>}
             />
           </div>
