1d6c978977cd47dc75020e2a146584869171fcf9
raeesapps <raees.aamir@yandex.com>
2019-03-25 18:22:33 +0000

    Added modal to gallery

--- src/newviews/Gallery/Gallery.jsx
+++ src/newviews/Gallery/Gallery.jsx
@@ -1,13 +1,41 @@
 import React, {useState} from "react";
 import Card from "../../components/Card/Card";
+import Modal from "../../components/Modal/Modal";
 import data from "../../assets/components/Grid/data";
 
 const Gallery = props => {
 
   const [viewImageModalOpen, viewImageModalVisible] = useState(false);
+  const [image, setImage] = useState(null);
+
+  const createViewImageModalContent = () => {
+    return (
+      <React.Fragment>
+        {image && <img style={{width: "100%"}} src={image.img} alt={image.title} /> }
+      </React.Fragment>
+    )
+  };
+
+  const createViewImageModalFooter = () => {
+    return (
+      <React.Fragment />
+    )
+  };
+
+  const onClickImage = image => {
+    setImage(image);
+    viewImageModalVisible(true);
+  };
 
   return (
     <div className="content">
+      <Modal
+        open={viewImageModalOpen}
+        close={() => viewImageModalVisible(false)}
+        title={"View Image"}
+        content={createViewImageModalContent()}
+        footer={createViewImageModalFooter()}
+        />
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-12">
@@ -16,7 +44,7 @@ const Gallery = props => {
               content={<div className="container-fluid">
                 <center><div className="row">
                   {
-                    data.map((image, idx) => <div key={idx} onClick={()=>console.log("clicked!")}className="col-md-6"><img style={{height: "50%", width: "80%", marginBottom: "25px"}} src={image.img} alt={image.title} /></div>)
+                    data.map((image, idx) => <div key={idx} onClick={()=>onClickImage(image)} className="col-md-6"><img style={{height: "50%", width: "80%", marginBottom: "25px"}} src={image.img} alt={image.title} /></div>)
                   }
                 </div></center>
               </div>}
