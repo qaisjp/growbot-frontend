e8dfd83e348ab6552201e91e56a8ac6bf1fab866
raeesapps <raees.aamir@yandex.com>
2019-03-30 16:45:53 +0000

    Removed unnecessary code from logging view

--- src/views/Logging/Logging.jsx
+++ src/views/Logging/Logging.jsx
@@ -1,16 +1,9 @@
 import React, { useState, useEffect } from "react";
-import moment from "moment";
 
 import Card from "../../components/Card/Card";
+import LoggingTable from "../../components/Logging/Logging";
 import httpFetchLogs from "../../http/fetch_logs";
 import { connect } from "react-redux";
-
-const severity = {
-  0: "info",
-  1: "success",
-  2: "warning",
-  3: "danger"
-};
 
 const Logging = props => {
 
@@ -47,28 +40,7 @@ const Logging = props => {
       <Card
         title={"Robot Logs"}
         content={
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Message</th>
-                <th>Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {logs.map(log => (
-                <tr className={severity[log.severity]}>
-                  <td>
-                    {log.message}
-                    <br />
-                    <strong>Robot: </strong>
-                    {getRobotName(log.robot_id)} <strong>Plant: </strong>
-                    {getPlantName(log.plant_id)}
-                  </td>
-                  <td>{moment(log.created_at).fromNow()}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <LoggingTable logs={logs} getPlantName={getPlantName} getRobotName={getRobotName} />
         }
       />
     </div>
