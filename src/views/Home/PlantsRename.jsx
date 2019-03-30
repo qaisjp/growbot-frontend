7723ac175b8b9550333375e4045ce32a4faaf2db
Qais Patankar <qaisjp@gmail.com>
2019-03-30 21:32:43 +0000

    Add missing PlantsRename file

---
+++ src/views/Home/PlantsRename.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Modal from "../../components/Modal/Modal.jsx";
+
+const PlantsAdd = ({ visible, onClose, onSubmit }) => {
+  const [name, setName] = useState("");
+
+  return (
+    <Modal
+      open={visible}
+      close={onClose}
+      content={
+        title = "Add Plant"
+        < div >
+        <p>Enter a new name for your plant</p>
+        <div className="form-group">
+          <label htmlFor="inputName">New Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputName"
+            placeholder="Name"
+            onChange={event => setName(event.target.value)}
+          />
+        </div>
+                </div >
+            }
+footer = {
+                <>
+  <button onClick={onClose} className="btn btn-danger">
+    Close
+          </button>
+  <button
+    onClick={onSubmit.bind(null, name)}
+    className="btn btn-danger"
+  >
+    Rename
+          </button>
+                </>
+            }
+/>
+    );
+};
+
+export default PlantsAdd;
