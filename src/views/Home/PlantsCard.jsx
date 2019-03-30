602aacbd216478ad82c5bdf66478d01e36386f03
Qais Patankar <qaisjp@gmail.com>
2019-03-30 21:31:32 +0000

    Format PlantsCard

--- src/views/Home/PlantsCard.jsx
+++ src/views/Home/PlantsCard.jsx
@@ -15,161 +15,157 @@ import PlantsRemove from "./PlantsRemove";
 import PlantsRename from "./PlantsRename"
 
 const PlantsCard = props => {
-  const [selectedPlant, selectPlant] = useState({});
-  const [addPlantModalOpen, addPlantModalVisible] = useState(false);
-  const [renamePlantModalOpen, renamePlantModalVisible] = useState(false);
-  const [removePlantModalOpen, removePlantModalVisible] = useState(false);
+    const [selectedPlant, selectPlant] = useState({});
+    const [addPlantModalOpen, addPlantModalVisible] = useState(false);
+    const [renamePlantModalOpen, renamePlantModalVisible] = useState(false);
+    const [removePlantModalOpen, removePlantModalVisible] = useState(false);
 
-  const { loginToken, plants, onPlantAdded } = props;
+    const { loginToken, plants, onPlantAdded } = props;
 
-  const reduxPlants = plants;
+    const reduxPlants = plants;
 
-  const onRenamePlant = async (renamePlantName) => {
-    console.log(reduxPlants);
-    if (renamePlantName === "") {
-      // todo: improve alert
-      alert("Please make sure you've typed in a new name!");
-      return;
-    }
+    const onRenamePlant = async (renamePlantName) => {
+        console.log(reduxPlants);
+        if (renamePlantName === "") {
+            // todo: improve alert
+            alert("Please make sure you've typed in a new name!");
+            return;
+        }
 
-    const { reduxRenamePlant } = props;
+        const { reduxRenamePlant } = props;
 
-    const response = await httpRenamePlant(
-      loginToken,
-      selectedPlant.id,
-      renamePlantName
-    );
+        const response = await httpRenamePlant(
+            loginToken,
+            selectedPlant.id,
+            renamePlantName
+        );
 
-    if (response.status === 200) {
-      reduxRenamePlant(selectedPlant, renamePlantName);
-    }
+        if (response.status === 200) {
+            reduxRenamePlant(selectedPlant, renamePlantName);
+        }
 
-    renamePlantModalVisible(false);
-  };
-  const onRemovePlant = async () => {
-    const { reduxRemovePlant } = props;
+        renamePlantModalVisible(false);
+    };
+    const onRemovePlant = async () => {
+        const { reduxRemovePlant } = props;
 
-    const response = await httpRemovePlant(loginToken, selectedPlant.id);
+        const response = await httpRemovePlant(loginToken, selectedPlant.id);
 
-    if (response.status === 200) {
-      reduxRemovePlant(selectedPlant);
-    }
+        if (response.status === 200) {
+            reduxRemovePlant(selectedPlant);
+        }
 
-    removePlantModalVisible(false);
-  };
-  const onAddPlant = async newPlantName => {
-    if (newPlantName === "") {
-      // todo: improve alert
-      alert("Please make sure you've entered a name for your new plant!");
-      return;
-    }
+        removePlantModalVisible(false);
+    };
+    const onAddPlant = async newPlantName => {
+        if (newPlantName === "") {
+            // todo: improve alert
+            alert("Please make sure you've entered a name for your new plant!");
+            return;
+        }
 
-    const response = await httpAddPlant(loginToken, newPlantName);
+        const response = await httpAddPlant(loginToken, newPlantName);
 
-    if (response.status === 200) {
-      onPlantAdded();
-    }
+        if (response.status === 200) {
+            onPlantAdded();
+        }
 
-    addPlantModalVisible(false);
-  };
+        addPlantModalVisible(false);
+    };
 
-  return (
-    <React.Fragment>
-      <Card
-        title={
-          <span
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center"
-            }}
-          >
-            <span>Your Plants</span>
-            <button
-              onClick={() => addPlantModalVisible(true)}
-              className="btn btn-sm btn-danger"
-            >
-              Add Plant
+    return (
+        <React.Fragment>
+            <Card
+                title={
+                    <span
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center"
+                        }}
+                    >
+                        <span>Your Plants</span>
+                        <button
+                            onClick={() => addPlantModalVisible(true)}
+                            className="btn btn-sm btn-danger"
+                        >
+                            Add Plant
             </button>
-          </span>
-        }
-        content={
-          <div>
-            <ul className="list-group">
-              {reduxPlants
-                .filter(plant => plant !== undefined)
-                .map((plant, idx) => (
-                  <li key={idx} className="list-group-item">
-                    {plant.name}{" "}
-                    <button
-                      onClick={() => {
-                        const plant = reduxPlants[idx];
-                        selectPlant(plant);
-                        removePlantModalVisible(true);
-                      }}
-                      type="button"
-                      style={{ marginLeft: "10px" }}
-                      className="btn btn-sm btn-danger pull-right"
-                    >
-                      <i className="glyphicon glyphicon-trash" />
-                    </button>{" "}
-                    <button
-                      onClick={() => {
-                        const plant = reduxPlants[idx];
-                        selectPlant(plant);
-                        renamePlantModalVisible(true);
-                      }}
-                      type="button"
-                      className="btn btn-sm btn-danger pull-right"
-                    >
-                      <i className="glyphicon glyphicon-pencil" />
-                    </button>
-                  </li>
-                ))}
-            </ul>
-          </div>
-        }
-      />
+                    </span>
+                }
+                content={
+                    <div>
+                        <ul className="list-group">
+                            {reduxPlants
+                                .filter(plant => plant !== undefined)
+                                .map((plant, idx) => (
+                                    <li key={idx} className="list-group-item">
+                                        {plant.name}{" "}
+                                        <button
+                                            onClick={() => {
+                                                const plant = reduxPlants[idx];
+                                                selectPlant(plant);
+                                                removePlantModalVisible(true);
+                                            }}
+                                            type="button"
+                                            style={{ marginLeft: "10px" }}
+                                            className="btn btn-sm btn-danger pull-right"
+                                        >
+                                            <i className="glyphicon glyphicon-trash" />
+                                        </button>{" "}
+                                        <button
+                                            onClick={() => {
+                                                const plant = reduxPlants[idx];
+                                                selectPlant(plant);
+                                                renamePlantModalVisible(true);
+                                            }}
+                                            type="button"
+                                            className="btn btn-sm btn-danger pull-right"
+                                        >
+                                            <i className="glyphicon glyphicon-pencil" />
+                                        </button>
+                                    </li>
+                                ))}
+                        </ul>
+                    </div>
+                }
+            />
 
-      <PlantsAdd
-        onSubmit={onAddPlant}
-        visible={addPlantModalOpen}
-        onClose={() => {
-          addPlantModalVisible(false);
-        }}
-      />
-      <PlantsRename
-        onSubmit={onRenamePlant}
-        visible={renamePlantModalOpen}
-        onClose={() => {
-            renamePlantModalVisible(false);
-        }}
-      />
-      <PlantsRemove
-        onSubmit={onRemovePlant}
-        visible={removePlantModalOpen}
-        onClose={() => removePlantModalVisible(false)}
-      />
-    </React.Fragment>
-  );
+            <PlantsAdd
+                onSubmit={onAddPlant}
+                visible={addPlantModalOpen}
+                onClose={() => addPlantModalVisible(false)}
+            />
+            <PlantsRename
+                onSubmit={onRenamePlant}
+                visible={renamePlantModalOpen}
+                onClose={() => renamePlantModalVisible(false)}
+            />
+            <PlantsRemove
+                onSubmit={onRemovePlant}
+                visible={removePlantModalOpen}
+                onClose={() => removePlantModalVisible(false)}
+            />
+        </React.Fragment>
+    );
 };
 
 const mapStateToProps = state => {
-  const {
-    auth: { loginToken },
-    plantState: { plants }
-  } = state;
-  return { plants, loginToken };
+    const {
+        auth: { loginToken },
+        plantState: { plants }
+    } = state;
+    return { plants, loginToken };
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    reduxRemovePlant: plant => dispatch(removePlant(plant)),
-    reduxRenamePlant: (plant, name) => dispatch(renamePlant(plant, name))
-  };
+    return {
+        reduxRemovePlant: plant => dispatch(removePlant(plant)),
+        reduxRenamePlant: (plant, name) => dispatch(renamePlant(plant, name))
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(PlantsCard);
