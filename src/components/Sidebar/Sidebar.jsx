1af2828a6e4663fa0bcf9644fc181f0f61ba4780
Qais Patankar <qaisjp@gmail.com>
2019-03-30 20:40:20 +0000

    Use an svg for the growbot logo instead

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
-import logo from "../../assets/img/logo.png";
+import logo from "../../assets/img/logo-white.svg";
 import dashboardRoutes from "../../routes/dashboard_routes";
 
 const Sidebar = props => {
