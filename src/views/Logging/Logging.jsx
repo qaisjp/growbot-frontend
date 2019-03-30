d42e34f9a4fd3d089f2f98384a2220f7805a989c
raeesapps <raees.aamir@yandex.com>
2019-03-30 14:04:05 +0000

    Added logs endpoint

--- src/views/Logging/Logging.jsx
+++ src/views/Logging/Logging.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import moment from "moment";
 
 import Card from "../../components/Card/Card";
+import httpFetchLogs from "../../http/fetch_logs";
+import { connect } from "react-redux";
 
-const Logging = () => {
+const Logging = props => {
+
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    fetchLogs()
+  }, []);
+
+  const getRobotName = id => {
+
+  };
+
+  const getPlantName = id => {
+
+  };
+
+  const fetchLogs = async () => {
+    const { loginToken } = props;
+    const fetchLogsResult = await httpFetchLogs(loginToken);
+
+    if(!(fetchLogsResult instanceof Error)) {
+      console.log(fetchLogsResult);
+      const {entries} = fetchLogsResult;
+      setLogs(entries);
+    }
+  };
+
   return (
     <div className="content">
       <Card
@@ -16,6 +45,16 @@ const Logging = () => {
               </tr>
             </thead>
             <tbody>
+            {logs.map(log => (
+              <tr className="success">
+                <td>
+                  {log.message}
+                  <br />
+                  <small>Robot: PrototypeBot · Plant: Sunflower</small>
+                </td>
+                <td>{moment(log.created_at).fromNow()}</td>
+              </tr>
+            ))}
               <tr className="danger">
                 <td>
                   GrowBot is stuck between two objects
@@ -26,7 +65,7 @@ const Logging = () => {
                     Sunflower
                   </small>
                 </td>
-                <td>2 minutes ago</td>
+                <td>3 hours ago</td>
               </tr>
               <tr className="success">
                 <td>
@@ -44,4 +83,22 @@ const Logging = () => {
   );
 };
 
-export default Logging;
+const mapStateToProps = props => {
+  const { plants } = props.plantState;
+  const { selectedRobot } = props.robotState;
+  const { loginToken } = props.auth;
+  return {
+    loginToken,
+    selectedRobot,
+    reduxPlants: plants
+  };
+};
+
+const mapDispatchToProps = () => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Logging);
