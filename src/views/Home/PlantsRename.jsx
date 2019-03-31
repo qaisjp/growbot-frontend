8488f97f2f6a97f83e63c1a64a5875106c58fbdf
Qais Patankar <qaisjp@gmail.com>
2019-03-31 14:58:44 +0100

    Fix PlantsRename (wtf?)

--- src/views/Home/PlantsRename.jsx
+++ src/views/Home/PlantsRename.jsx
@@ -8,37 +8,37 @@ const PlantsAdd = ({ visible, onClose, onSubmit }) => {
     <Modal
       open={visible}
       close={onClose}
+      title={"Add Plant"}
       content={
-        title = "Add Plant"
-        < div >
-        <p>Enter a new name for your plant</p>
-        <div className="form-group">
-          <label htmlFor="inputName">New Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="inputName"
-            placeholder="Name"
-            onChange={event => setName(event.target.value)}
-          />
-        </div>
-                </div >
-            }
-footer = {
-                <>
-  <button onClick={onClose} className="btn btn-danger">
-    Close
+        <div>
+          <p>Enter a new name for your plant</p>
+          <div className="form-group">
+            <label htmlFor="inputName">New Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputName"
+              placeholder="Name"
+              onChange={event => setName(event.target.value)}
+            />
+          </div>
+        </div >
+      }
+      footer={
+        <>
+          <button onClick={onClose} className="btn btn-danger">
+            Close
           </button>
-  <button
-    onClick={onSubmit.bind(null, name)}
-    className="btn btn-danger"
-  >
-    Rename
+          <button
+            onClick={onSubmit.bind(null, name)}
+            className="btn btn-danger"
+          >
+            Rename
           </button>
-                </>
-            }
-/>
-    );
+        </>
+      }
+    />
+  );
 };
 
 export default PlantsAdd;
