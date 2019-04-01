0501dfe4904631cb90e1baf45566a692230b4093
Qais Patankar <qaisjp@gmail.com>
2019-04-01 17:28:58 +0100

    plants: add qr code printing feature

--- src/views/Home/PlantsPrint.jsx
+++ src/views/Home/PlantsPrint.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, {useRef} from "react";
 import Modal from "../../components/Modal/Modal.jsx";
 import QRCode from "qrcode.react";
+import ReactToPrint from 'react-to-print';
+
 
 const PlantsPrint = ({ visible, onClose, onSubmit, plant }) => {
     if (!visible) {
         return null;
     }
+
+    // From https://stackoverflow.com/a/18500019/1517394
+    const times = function(n, iterator) {
+        var accum = Array(Math.max(0, n));
+        for (var i = 0; i < n; i++) accum[i] = iterator.call();
+        return accum;
+    };
+
+    const qrCode = useRef();
+    const printed = (
+        <div ref={qrCode}>
+            {
+                times(6,
+                    () => <QRCode value={`gbpl:${plant.id}`} level="H" size={925} includeMargin={true} style={{borderRight: "dashed black 20px"}} />
+                )
+            }
+        </div>
+    )
 
     return (
         <Modal
@@ -15,10 +35,15 @@ const PlantsPrint = ({ visible, onClose, onSubmit, plant }) => {
             content={
                 <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                     <QRCode value={`gbpl:${plant.id}`} level="H" size={200} />
+                    <div style={{display: "none", width: "100%", height: "100%", justifyContent: "center", alignItems: "center"}}>{printed}</div>
                 </div>
             }
             footer={
                 <>
+                    <ReactToPrint
+                        trigger={() => <button className="btn btn-info">Print</button>}
+                        content={() => qrCode.current}
+                    />
                     <button onClick={onClose} className="btn btn-danger">
                         Close
                     </button>
