8967229d531b9fda85a096d1e68b4dfc120fa247
Qais Patankar <qaisjp@gmail.com>
2019-04-02 21:04:21 +0100

    sched: take footer out of modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Modal = props => {
-    const {open, close, title, content, footer, ...otherProps} = props;
+    const {open, close, title, content, footer, full=false, ...otherProps} = props;
     const modalClassAttribute = open ? "show" : "hide";
 
     return (
@@ -18,8 +18,10 @@ const Modal = props => {
                         </button>
                         <h4 className="modal-title">{title}</h4>
                     </div>
-                    <div className="modal-body">{open && content}</div>
+                    {full ? <content /> : <>
+                    <div className="modal-body">{content}</div>
                     <div className="modal-footer">{footer}</div>
+                    </>}
                 </div>
             </div>
         </div>
--- src/views/Scheduler/NewEvent.jsx
+++ src/views/Scheduler/NewEvent.jsx
@@ -14,7 +14,7 @@ import httpScheduleAction from "../../http/schedule_action";
 import getRRule from "./scheduler_get_rrule";
 import units from "./scheduler_time_units";
 
-const Content = ({loginToken, plants, robots, shouldSubmit, onSubmit, ...props}) => {
+const Content = ({loginToken, plants, robots, onClose, onSubmit, ...props}) => {
     const actionNames = actions.map(action => action.name);
     const plantNames = plants.map(plant => plant.name);
     const reduxRobotNames = robots.map(robot => robot.title);
@@ -95,191 +95,188 @@ const Content = ({loginToken, plants, robots, shouldSubmit, onSubmit, ...props})
         onSubmit();
     }
 
-    useEffect(() => {
-        if (shouldSubmit) {
-            onSchedule();
-        }
-    }, [shouldSubmit])
+
+    return (<>
+        <div className="modal-body">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-6">
+                        <label>Summary</label>
+                        <input
+                            onChange={event => setSummary(event.target.value)}
+                            style={{marginLeft: "10px"}}
+                            type="text"
+                        />
+                        <div style={{marginTop: "10px"}}/>
+                        <label style={{marginRight: "10px"}}>Start date</label>
+                        <Datetime onChange={setStartDate} value={startDate}/>
+                        <div style={{marginTop: "10px"}}/>
+                        <div style={{display: "inline"}}>
+                        <label>Robots</label>
+                        <Dropdown
+                            name="Robots"
+                            style={{display: "inline", marginLeft: "10px"}}
+                            items={reduxRobotNames}
+                            click={robotName => {
+                                const idx = reduxRobotNames.indexOf(robotName);
+                                selectRobot(robots[idx]);
+                            }}
+                        />
+                        <div style={{marginTop: "10px"}}/>
+
+                        <label>Plant</label>
+                        <Dropdown
+                            name="Plants"
+                            style={{display: "inline", marginLeft: "10px"}}
+                            items={plantNames}
+                            click={plantName => {
+                                const idx = plantNames.indexOf(plantName);
+                                selectPlant(plants[idx]);
+                            }}
+                        />
+                        <div style={{marginTop: "10px"}}/>
+                        <label>
+                            Action
+                        </label>
+                        <Dropdown
+                            name="Actions"
+                            style={{
+                                display: "inline", marginLeft: "10px"
+                            }}
+                            items={actionNames}
+                            click={actionName => {
+                                if(plant && robot) {
+                                    const idx = actionNames.indexOf(actionName);
+                                    eventActions.push({
+                                        name: actions[idx].name, type: actions[idx].type, robot_id: robot.id, plant_id: plant.id
+                                    });
+                                    selectAction(actions[idx]);
+                                }
+                            }}
+                        />
+                        </div>
+                        <div style={{marginTop: "10px"}}/>
+                        <label>Repeat</label>
+                        <input
+                            style={{
+                                marginLeft: "10px",
+                                width: "30%",
+                                height: "29px",
+                                display: "inline-block"
+                            }}
+                            type="number"
+                            className="form-control"
+                            onChange={event => setRepeatEveryNumber(event.target.value)}
+                        />
+                        <Dropdown
+                            name="Time"
+                            style={{display: "inline", marginLeft: "10px"}}
+                            items={units}
+                            click={unit => {
+                                const idx = units.indexOf(unit);
+                                setRepeatEveryUnit(units[idx]);
+                            }}
+                        />
+                        <div style={{marginTop: "10px"}}/>
+                        <label style={{display: eventActions.length ? "block" : "none"}}>
+                            Actions
+                        </label>
+                        <div style={{marginTop: "10px"}}/>
+                        <ul
+                            style={{display: eventActions.length ? "inline" : "none"}}
+                            className="list-group"
+                        >
+                            {eventActions.map((action, idx) => (
+
+                                <li className="list-group-item">
+                                    {idx + 1 + ". " + action.name}
+
+                                    <button
+                                        onClick={() => {
+                                            const updatedEventActions = eventActions.filter((action, selectedIdx) => selectedIdx !== idx);
+                                            setEventActions(updatedEventActions);
+                                        }}
+                                        style={{marginLeft:"10px"}}
+                                        type="button"
+                                        className="btn btn-sm btn-danger"
+                                    >
+                                        <i className="glyphicon glyphicon-trash" />
+                                    </button>
+                                </li>
 
 
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-6">
-                    <label>Summary</label>
-                    <input
-                        onChange={event => setSummary(event.target.value)}
-                        style={{marginLeft: "10px"}}
-                        type="text"
-                    />
-                    <div style={{marginTop: "10px"}}/>
-                    <label style={{marginRight: "10px"}}>Start date</label>
-                    <Datetime onChange={setStartDate} value={startDate}/>
-                    <div style={{marginTop: "10px"}}/>
-                    <div style={{display: "inline"}}>
-                    <label>Robots</label>
-                    <Dropdown
-                        name="Robots"
-                        style={{display: "inline", marginLeft: "10px"}}
-                        items={reduxRobotNames}
-                        click={robotName => {
-                            const idx = reduxRobotNames.indexOf(robotName);
-                            selectRobot(robots[idx]);
-                        }}
-                    />
-                    <div style={{marginTop: "10px"}}/>
-
-                    <label>Plant</label>
-                    <Dropdown
-                        name="Plants"
-                        style={{display: "inline", marginLeft: "10px"}}
-                        items={plantNames}
-                        click={plantName => {
-                            const idx = plantNames.indexOf(plantName);
-                            selectPlant(plants[idx]);
-                        }}
-                    />
-                    <div style={{marginTop: "10px"}}/>
-                    <label>
-                        Action
-                    </label>
-                    <Dropdown
-                        name="Actions"
-                        style={{
-                            display: "inline", marginLeft: "10px"
-                        }}
-                        items={actionNames}
-                        click={actionName => {
-                            if(plant && robot) {
-                                const idx = actionNames.indexOf(actionName);
-                                eventActions.push({
-                                    name: actions[idx].name, type: actions[idx].type, robot_id: robot.id, plant_id: plant.id
-                                });
-                                selectAction(actions[idx]);
-                            }
-                        }}
-                    />
+                            ))}
+                        </ul>
                     </div>
-                    <div style={{marginTop: "10px"}}/>
-                    <label>Repeat</label>
-                    <input
-                        style={{
-                            marginLeft: "10px",
-                            width: "30%",
-                            height: "29px",
-                            display: "inline-block"
-                        }}
-                        type="number"
-                        className="form-control"
-                        onChange={event => setRepeatEveryNumber(event.target.value)}
-                    />
-                    <Dropdown
-                        name="Time"
-                        style={{display: "inline", marginLeft: "10px"}}
-                        items={units}
-                        click={unit => {
-                            const idx = units.indexOf(unit);
-                            setRepeatEveryUnit(units[idx]);
-                        }}
-                    />
-                    <div style={{marginTop: "10px"}}/>
-                    <label style={{display: eventActions.length ? "block" : "none"}}>
-                        Actions
-                    </label>
-                    <div style={{marginTop: "10px"}}/>
-                    <ul
-                        style={{display: eventActions.length ? "inline" : "none"}}
-                        className="list-group"
-                    >
-                        {eventActions.map((action, idx) => (
+                    <div className="col-md-6">
+                        <label style={{display: "block"}}>Repeat on</label>
+                        <div style={{marginTop: "10px"}}/>
+                        {createRepeatOnCheckbox(MONDAY)}
+                        {createRepeatOnCheckbox(TUESDAY)}
+                        {createRepeatOnCheckbox(WEDNESDAY)}
+                        {createRepeatOnCheckbox(THURSDAY)}
+                        {createRepeatOnCheckbox(FRIDAY)}
+                        {createRepeatOnCheckbox(SATURDAY)}
+                        {createRepeatOnCheckbox(SUNDAY)}
+                        <div style={{marginTop: "10px"}}/>
+                        <label style={{display: "block"}}>Ends</label>
+                        <div style={{marginTop: "10px"}}/>
+                        <label style={{marginRight: "10px"}}>Never</label>
+                        <input
+                            type="radio"
+                            checked={ends === NEVER}
+                            onClick={() => setEnds(NEVER)}
+                        />
+                        <div style={{marginTop: "10px"}}/>
 
-                            <li className="list-group-item">
-                                {idx + 1 + ". " + action.name}
-
-                                <button
-                                    onClick={() => {
-                                        const updatedEventActions = eventActions.filter((action, selectedIdx) => selectedIdx !== idx);
-                                        setEventActions(updatedEventActions);
-                                    }}
-                                    style={{marginLeft:"10px"}}
-                                    type="button"
-                                    className="btn btn-sm btn-danger"
-                                >
-                                    <i className="glyphicon glyphicon-trash" />
-                                </button>
-                            </li>
-
-
-                        ))}
-                    </ul>
-                </div>
-                <div className="col-md-6">
-                    <label style={{display: "block"}}>Repeat on</label>
-                    <div style={{marginTop: "10px"}}/>
-                    {createRepeatOnCheckbox(MONDAY)}
-                    {createRepeatOnCheckbox(TUESDAY)}
-                    {createRepeatOnCheckbox(WEDNESDAY)}
-                    {createRepeatOnCheckbox(THURSDAY)}
-                    {createRepeatOnCheckbox(FRIDAY)}
-                    {createRepeatOnCheckbox(SATURDAY)}
-                    {createRepeatOnCheckbox(SUNDAY)}
-                    <div style={{marginTop: "10px"}}/>
-                    <label style={{display: "block"}}>Ends</label>
-                    <div style={{marginTop: "10px"}}/>
-                    <label style={{marginRight: "10px"}}>Never</label>
-                    <input
-                        type="radio"
-                        checked={ends === NEVER}
-                        onClick={() => setEnds(NEVER)}
-                    />
-                    <div style={{marginTop: "10px"}}/>
-
-                    <label style={{marginRight: "10px"}}>On</label>
-                    <input
-                        type="radio"
-                        style={{marginRight: "10px"}}
-                        checked={ends === ON}
-                        onClick={() => setEnds(ON)}
-                    />
-                    <Datetime onChange={setDate} value={date}/>
-                    <div style={{marginTop: "10px"}}/>
-                    <label style={{marginRight: "10px"}}>After</label>
-                    <input
-                        type="radio"
-                        style={{marginRight: "10px"}}
-                        checked={ends === AFTER}
-                        onClick={() => setEnds(AFTER)}
-                    />
-                    <input
-                        style={{width: "30%", height: "29px", display: "inline-block"}}
-                        type="number"
-                        className="form-control"
-                        onChange={event => setAfterOccurances(event.target.value)}
-                    />
+                        <label style={{marginRight: "10px"}}>On</label>
+                        <input
+                            type="radio"
+                            style={{marginRight: "10px"}}
+                            checked={ends === ON}
+                            onClick={() => setEnds(ON)}
+                        />
+                        <Datetime onChange={setDate} value={date}/>
+                        <div style={{marginTop: "10px"}}/>
+                        <label style={{marginRight: "10px"}}>After</label>
+                        <input
+                            type="radio"
+                            style={{marginRight: "10px"}}
+                            checked={ends === AFTER}
+                            onClick={() => setEnds(AFTER)}
+                        />
+                        <input
+                            style={{width: "30%", height: "29px", display: "inline-block"}}
+                            type="number"
+                            className="form-control"
+                            onChange={event => setAfterOccurances(event.target.value)}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
-    );
+
+        <div className="modal-footer">
+            <button onClick={onClose} className="btn btn-secondary">
+                Close
+            </button>
+            <button onClick={onSchedule()} className="btn btn-primary">
+                Schedule
+            </button>
+        </div>
+
+    </>);
 }
 
 const NewEvent = ({visible, onClose, onSubmit, ...props}) => {
-    const [shouldSubmit, setShouldSubmit] = useState(false);
     return (
         <Modal
                 open={visible}
                 close={onClose}
                 title="Scheduler"
-                content={<Content shouldSubmit={shouldSubmit} onSubmit={onSubmit} {...props} />}
-                footer={
-                    <>
-                        <button onClick={onClose} className="btn btn-secondary">
-                            Close
-                        </button>
-                        <button onClick={() => {setShouldSubmit(true)}} className="btn btn-primary">
-                            Schedule
-                        </button>
-                    </>
-                }
+                full={true}
+                content={<Content onClose={onClose} onSubmit={onSubmit} {...props} />}
+                footer={null}
             />
     )
 }
